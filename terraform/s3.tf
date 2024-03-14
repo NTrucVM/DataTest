a5f358e555b3e4de8184821127af222e54162458
Truc Vo <nhatrucvominh@gmail.com>
2024-03-14 22:43:16 +0700

    Test 2

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "example" {
-  bucket = "qa-community-${var.TrucVMN1}-s3"
+  bucket = "qa-community-${var.your_account}-s3"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
